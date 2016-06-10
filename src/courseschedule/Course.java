f03d7441e6851e962c1e99c7867e100497245466
sachdatta <sach.dut@gmail.com>
2016-06-10 01:43:00 -0500

    initial commit

---
+++ src/courseschedule/Course.java
@@ -0,0 +1,58 @@
+public class Course {
+
+	private string name;
+	private string number;
+	private string description;
+	private string capacity;
+	private string prerequisite;
+	private Semester semester;
+
+	public string getName() {
+		return this.name;
+	}
+
+	public void setName(string name) {
+		this.name = name;
+	}
+
+	public string getNumber() {
+		return this.number;
+	}
+
+	public void setNumber(string number) {
+		this.number = number;
+	}
+
+	public string getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(string description) {
+		this.description = description;
+	}
+
+	public string getCapacity() {
+		return this.capacity;
+	}
+
+	public void setCapacity(string capacity) {
+		this.capacity = capacity;
+	}
+
+	public string getPrerequisite() {
+		return this.prerequisite;
+	}
+
+	public void setPrerequisite(string prerequisite) {
+		this.prerequisite = prerequisite;
+	}
+
+	public Semester getSemester() {
+		return this.semester;
+	}
+
+	public void setSemester(Semester semester) {
+		this.semester = semester;
+	}
+
+}
