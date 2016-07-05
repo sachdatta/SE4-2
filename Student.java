5074661a4486bd0d3c24017f4f36f5b534e9876b
kishorekunapareddy <kishore.kunapareddy@eagles.oc.edu>
2016-07-05 18:31:09 -0500

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,29 @@
+package com.importfile;
+
+public class Student {
+	private String studentId;
+	private String degreeID;
+	private String yearSem;
+	
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String string) {
+		this.studentId = string;
+	}
+	public String getDegreeID() {
+		return degreeID;
+	}
+	public void setDegreeID(String string) {
+		this.degreeID = string;
+	}
+	public String getYearSem() {
+		return yearSem;
+	}
+	public void setYearSem(String yearSem) {
+		this.yearSem = yearSem;
+	}
+	
+	
+}
---
+++ StudentCourse.java
@@ -0,0 +1,45 @@
+package com.importfile;
+
+public class StudentCourse {
+	private String studentId;
+	private String courseId;
+	private String courseName;
+	private String yearSem;
+	private String grade;
+	
+	
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String string) {
+		this.studentId = string;
+	}
+	public String getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(String courseId) {
+		this.courseId = courseId;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	public String getYearSem() {
+		return yearSem;
+	}
+	public void setYearSem(String yearSem) {
+		this.yearSem = yearSem;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	
+	
+	
+
+}
